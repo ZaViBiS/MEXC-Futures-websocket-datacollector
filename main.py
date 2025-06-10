7d74f2d78d1e207796d2da6e130c36b8b3e1f631
ygryk <ygryk>
2025-06-10 16:39:29 +0200

    обробку помилок підключення

--- main.py
+++ main.py
@@ -19,34 +19,32 @@ db = DB()
 
 
 async def connect():
-    async with websockets.connect(URI, ping_interval=None) as web:
-        await web.send(json.dumps(SUB_DEAL))
-        await web.send(json.dumps(SUB_FUND))
-        # await web.send(json.dumps(SUB_DEPTH))
-        start = 0
+    while True:
+        try:
+            async with websockets.connect(URI, ping_interval=None) as web:
+                await web.send(json.dumps(SUB_DEAL))
+                await web.send(json.dumps(SUB_FUND))
+                # await web.send(json.dumps(SUB_DEPTH))
+                start = 0
 
-        while True:
-            r = await web.recv()
-            data = json.loads(r)
-            print(data)
-            q.put(data)
-            # if data["channel"] == "push.deal":
-            #     db.add(data["data"])
-            # if data["channel"] == "push.funding.rate":
-            #     db.add_rate(data)
+                while True:
+                    try:
+                        r = await web.recv()
+                        data = json.loads(r)
+                        print(data)
+                        q.put(data)
 
-            # ping-pong
-            if time.time() - start > 10:
-                await web.send(json.dumps({"method": "ping"}))
-                start = time.time()
+                        # ping-pong
+                        if time.time() - start > 10:
+                            await web.send(json.dumps({"method": "ping"}))
+                            start = time.time()
+                    except Exception as e:
+                        print(f"виникла помилка під час отримання даних: {e}")
+        except Exception as e:
+            print(f"виникла помилка під час підключення: {e}")
 
 
 t = threading.Thread(target=db.loop)
 t.start()
 
-
-try:
-    while True:
-        asyncio.run(connect())
-except Exception as e:
-    print(e)
+asyncio.run(connect())
